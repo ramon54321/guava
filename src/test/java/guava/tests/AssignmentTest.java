b288760fdff2902c9f66ae0ba0b69f2863dffd93
anton5 <anton5@users.noreply.github.com>
2017-05-04 10:19:41 +0300

    Added more tests.

--- src/test/java/guava/tests/AssignmentTest.java
+++ src/test/java/guava/tests/AssignmentTest.java
@@ -7,8 +7,32 @@ import static org.junit.Assert.*;
 public class AssignmentTest {
 
     @Test
-    public void testConstructor() {
+    public void testId() {
 	Assignment a = new Assignment(1, 2, 3, "2017-06-01", "Exercise 1", "", 5);
 	assertEquals(1, a.getId());
+    }
+
+    @Test
+    public void testCourseId() {
+	Assignment a = new Assignment(1, 2, 3, "2017-06-01", "Exercise 1", "", 5);
+	assertEquals(2, a.getCourseId());
+    }
+
+    @Test
+    public void testTeacherUserId() {
+	Assignment a = new Assignment(1, 2, 3, "2017-06-01", "Exercise 1", "", 5);
+	assertEquals(3, a.getTeacherUserId());
+    }
+
+    @Test
+    public void testDeadline() {
+	Assignment a = new Assignment(1, 2, 3, "2017-06-01", "Exercise 1", "", 5);
+	assertEquals("2017-06-01", a.getDeadline());
+    }
+
+    @Test
+    public void testTitle() {
+	Assignment a = new Assignment(1, 2, 3, "2017-06-01", "Exercise 1", "", 5);
+	assertEquals("Exercise 1", a.getTitle());
     }
 }
