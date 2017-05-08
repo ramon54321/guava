7c0c7bc41d9bf8936d7119657d2917bcd55946f0
anton5 <anton5@users.noreply.github.com>
2017-05-08 15:03:40 +0300

    Updated Course and Assignment methods of REST interface.

--- src/main/java/guava/rest/Course.java
+++ src/main/java/guava/rest/Course.java
@@ -1,12 +1,21 @@
 package guava.rest;
 
+import java.util.ArrayList;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/course")
 public class Course {
     @GET
-    public String getAllCourses() {
-	return "Version 1";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllCourses() {
+	ArrayList<guava.model.Course> a = new ArrayList<>();
+	a.add(new guava.model.Course(1, "Finnish 1"));
+	a.add(new guava.model.Course(2, "Finnish 2"));
+	a.add(new guava.model.Course(3, "Finnish 3"));
+	return Response.ok(a).build();
     }
 }
