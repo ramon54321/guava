e292ee40728f5d82aff8e1356b96531653591991
anton5 <anton5@users.noreply.github.com>
2017-05-10 13:43:47 +0300

    Added method grade for Submission.

--- src/main/java/guava/rest/Submission.java
+++ src/main/java/guava/rest/Submission.java
@@ -5,7 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -17,14 +19,22 @@ public class Submission {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllSubmissions() {
-    	ArrayList<guava.model.Submission> a = new ArrayList<>();
-    	a.add(new guava.model.Submission(1, 1, 1, "Some answer", "2017-06-01", 3, ""));
-    	return Response.ok(a).build();
+	ArrayList<guava.model.Submission> a = new ArrayList<>();
+	a.add(new guava.model.Submission(1, 1, 1, "Some answer", "2017-06-01", 3, ""));
+	return Response.ok(a).build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response postSubmission(Submission submission, @Context HttpServletRequest request) {
-    	return Response.ok().build();
+	return Response.ok().build();
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}/grade/{value}")
+    public Response gradeSubmission(@PathParam("id") String id, @PathParam("value") String value, @Context HttpServletRequest request) {
+	return Response.ok().build();
     }
 }
